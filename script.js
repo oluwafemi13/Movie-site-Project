be79255dd244da00e46dbfe5071b2576937e1f41
oluwafemi13 <sundayjabikem@gmail.com>
2022-08-20 23:55:08 +0100

    added api key

---
+++ script.js
@@ -0,0 +1,3 @@
+const API_URl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5461e2170c497477bbd7f56bd52d33b2&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=5461e2170c497477bbd7f56bd52d33b2&query="';
